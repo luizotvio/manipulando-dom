e61c940f7f4a2977f8640658568364b85f9c10b2
luizotvio <luizotavio.03@outlook.com>
2024-06-06 11:11:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let titulo = document.getElementById('titulo').innerText = 'Colocando o título com JavaScript';
+
+
+let listaNaoOrdenada = document.querySelector('ul').innerHTML = `
+    <li>usando ID</li>
+    <li>querySelector</li>
+    <li>innerHTML e innerText</li>
+`;
+
+let link = document.querySelector('a').innerText = 'Site - Proz Educação'
+
+let listaOrdenada = document.getElementById('lista-ordenada').innerHTML = `
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+`
